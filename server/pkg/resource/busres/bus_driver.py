916d0a7fd1fa41a36b6f0b97316f568f75f297fb
ting96haha <chaiyeeting96@gmail.com>
2019-01-15 19:10:46 +0800

    -Minor Changes

--- server/pkg/resource/busres/bus_driver.py
+++ server/pkg/resource/busres/bus_driver.py
@@ -71,7 +71,6 @@ class Bus_Driver_EditForm(r.FlaskForm):
     # The names here after the rgen_ prefix must correspond to a var name in the respective model
     rgen_name = r.StringField('Renew Name',validators=[r.InputRequired(),r.Length(min=4,max=40)])
     rgen_contact_no = r.StringField('Renew Contact Number',validators=[r.InputRequired(),r.Length(min=10,max=11)])
-    rgen_password = r.PasswordField('New Password',validators=[r.InputRequired(),r.Length(min=5,max=80)])
     #TODO: List select fields here, FIELDS MUST BE PREFIXED WITH rgensel_
     # The names here after the rgen_ prefix must correspond to a var name in the respective model
     fKeylist = {}
