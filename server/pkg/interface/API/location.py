2f8cfd75d56bd63334a9dd517eb58418d25115d3
ting96haha <chaiyeeting96@gmail.com>
2019-01-26 13:25:43 +0800

    bug fix on logout

--- server/pkg/interface/API/location.py
+++ server/pkg/interface/API/location.py
@@ -74,8 +74,8 @@ def busLocAPI():
     #target_list[0] = active_bus.Active_Bus.query.filter(active_bus.Active_Bus.bus_id == upload_bufferArr[0]).first()
     if (target_list == None):
         #add
-        insert_bus_log = { "start_ts": start_time, "end_ts": start_time, "bus_id": upload_bufferArr[0], "driver_id": upload_bufferArr[1], "activebus_id": 99, "route_num":upload_bufferArr[2]}
-        target_bus_log = buslog.Bus_Log(insert_bus_log)
+        #insert_bus_log = { "start_ts": start_time, "end_ts": start_time, "bus_id": upload_bufferArr[0], "driver_id": upload_bufferArr[1], "activebus_id": 99, "route_num":upload_bufferArr[2]}
+        #target_bus_log = .Bus_Log(insert_bus_log)
 
         insert_list = { "bus_id":upload_bufferArr[0],"driver_id":upload_bufferArr[1],"route_num":upload_bufferArr[2],"time_stamp":start_time,"long":upload_bufferArr[3], "lati":upload_bufferArr[4], "current_seqno": 1}
         target_add = active_bus.Active_Bus(insert_list)
@@ -84,8 +84,8 @@ def busLocAPI():
             sq.db_session.add(target_add)
             sq.db_session.commit()
 
-            sq.db_session.add(target_bus_log) #problem always create buslog table once goes here !!!
-            sq.db_session.commit()
+            #sq.db_session.add(target_bus_log) #problem always create buslog table once goes here !!!
+            #sq.db_session.commit()
             ab_busid = active_bus.Active_Bus.query.filter(active_bus.Active_Bus.driver_id == upload_bufferArr[1]).first()
             curtime =datetime.datetime.now()
             insert_list= { "activebus_id":ab_busid.id,"time_stamp":curtime,"long":upload_bufferArr[3], "lati":upload_bufferArr[4]}
@@ -186,47 +186,64 @@ def buslogoutAPI():
             if( 'f'+str(idx) not in request.args):
                 return ("Missing argument "+'f'+str(idx))
 
+        #upload_buffer[0] = driver_id
         upload_targetArr=[]
         for idx in range(upload_argTotal):
             upload_targetArr.append(request.args.get('f'+str(idx)))
             print('f'+str(idx)+"="+request.args.get('f'+str(idx)),end=' ') #DEBUGGING ONLY
 
-        print() #DEBUGGING ONLY
+        #print() #DEBUGGING ONLY
+        target_del = active_bus.Active_Bus.query.filter(upload_targetArr[0] == active_bus.Active_Bus.driver_id).first()
+        #busdetails = buslog.Bus_Log.query.filter(upload_targetArr[0] == buslog.Bus_Log.driver_id).first()
         timestop = datetime.datetime.now()
-        target_del = active_bus.Active_Bus.query.filter(active_bus.Active_Bus.driver_id == upload_targetArr[0]).first()
-        #starttime = active_bus.Active_Bus.get(0)
-        #starttime = timenow()
-        #start_time = use_timenow()
-        busdetails = buslog.Bus_Log.query.filter(buslog.Bus_Log.driver_id == upload_targetArr[0]).first()
-        #databuslog = []
-        #databuslog[0] = get_starttime.start_ts
-        #databuslog[1] = timestop
-        #databuslog[2] = target_del.bus_id
-        #databuslog[3] = target_del.driver_id
-        #databuslog[4] = target_del.id
-        #databuslog[5] = target_del.route_num
-        if busdetails == None:
+        #if target_del.current_seqno == 1:
+        insert_list = {"start_ts":target_del.time_stamp,"end_ts":timestop, "bus_id":target_del.bus_id,"driver_id":target_del.driver_id,"activebus_id":target_del.id,"route_num":target_del.route_num}
+        target_add = buslog.Bus_Log(insert_list)
+        try:
+            sq.db_session.add(target_add)
+            sq.db_session.commit()
+            sq.db_session.delete(target_del)
+            sq.db_session.commit()
+                #srvlog["oper"].info("push/generic/param3 ADD :"+str(upload_bufferArr))
+            return '0'
+        except Exception as e:
+            sq.db_session.rollback()
+                #srvlog["oper"].error("push/generic/param3 FAIL :"+str(upload_bufferArr))
             return '1'
-        busdetails.end_ts = timestop
-        busdetails.bus_id = target_del.bus_id
-        busdetails.driver_id = target_del.driver_id
-        busdetails.activebus_id = target_del.id
-        busdetails.route_num = target_del.route_num
+        #if busdetails == None:
+         #  return '1'
+
+        #busdetails.start_ts = target_del.time_stamp
+        #busdetails.end_ts = timestop
+        #busdetails.bus_id = target_del.bus_id
+        #busdetails.driver_id = target_del.driver_id
+        #busdetails.route_num = target_del.route_num
+
+        #try:
+        #    sq.db_session.add(busdetails)
+        #    sq.db_session.commit()
+        #    sq.db_session.delete(target_del)
+        #    sq.db_session.commit()
+        #    return '0'
+
+        #except Exception as e:
+        #    sq.db_session.rollback()
+        #    return '1'
+
 
         #insert_log = { "start_ts": databuslog[0], "end_ts": databuslog[1], "bus_id": databuslog[2], "driver_id": databuslog[3], "activebus_id": databuslog[4], "route_num":databuslog[5]}
         #busdetails = buslog.Bus_Log(insert_log)
-        try:
+    #    try:
             #sq.db_session.add(busdetails)
             #sq.db_session.commit()
 
-            sq.db_session.delete(target_del)
-            sq.db_session.commit()
-            return "logout"
+        #    sq.db_session.delete(target_del)
+        #    sq.db_session.commit()
+        #    return "logout"
 
-        except Exception as e:
-            sq.db_session.rollback()
-            return "fail to add to bus log / delete table"
-
+    #    except Exception as e:
+    #        sq.db_session.rollback()
+    #        return "fail to add to bus log / delete table"
 
 ############################################################################################################################
 ##@bp.route('/bus/location/add')
